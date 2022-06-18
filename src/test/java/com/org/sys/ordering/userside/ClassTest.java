a9af8e3528550b5ad6927d70e9224e01ac3a05f8
Eli James Aguiar <eljames4@hotmail.com>
2022-06-18 16:29:11 -0300

    Jacoco report

---
+++ src/test/java/com/org/sys/ordering/userside/ClassTest.java
@@ -0,0 +1,15 @@
+package com.org.sys.ordering.userside;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ClassTest {
+
+    @Test
+    void test() {
+        Assertions.assertTrue(true);
+    }
+}
